6a73157aec11f038a79857f03b0f975a7c538ff7
David Acosta <davidacosta25081@yahoo.com>
2019-11-27 05:45:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,13 @@ function BoardMember(name,homeState,training) {
 }
 
 BoardMember.prototype.veto = function() {
-  return 'No, I must disagree'
+  return 'No, I must disagree' ;
+}  
+
+BoardMember.prototype.approve = function() {
+  return 'You can do that!' ;
+}  
+
+BoardMember.prototype.doCharity = function() {
+  return 'I like to help people.' ;
 }  
